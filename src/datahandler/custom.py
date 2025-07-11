6ff11a4dd139359ca7c864bd901c868ff5faa079
filomba01 <f.balzarini@an4a.com>
2025-07-11 15:48:01 +0200

    fix: dealing with splitted datased

--- src/datahandler/custom.py
+++ src/datahandler/custom.py
@@ -40,22 +40,34 @@ class SatelliteImages(DenoiseDataSet):
 class prep_SatelliteImages(DenoiseDataSet):
     '''
     dataset class for prepared satellite images which are cropped with overlap.
+    Added "split" argument (train/val/test) to support different dataset splits 
+    without creating separate classes.
     '''
     def __init__(self, *args, **kwargs):
+        # Extract split keyword (default: 'train') before calling parent constructor
+        self.split = kwargs.pop('split', 'train')  # train | val | test
+
+        # call parent constructor with remaining kwargs
         super().__init__(*args, **kwargs)
 
     def _scan(self):
-        self.dataset_path = os.path.join(self.dataset_dir, 'prep/SatelliteImages_s512_o128')
+        # Dataset is expected to be located under
+        #   ./dataset/prep/SatelliteImages_s512_o128/<split>/CL/*.png
+        # where <split> is train, val or test
+
+        self.dataset_path = os.path.join(self.dataset_dir, 'prep', 'SatelliteImages_s512_o128', self.split)
         assert os.path.exists(self.dataset_path), 'There is no dataset %s'%self.dataset_path
-        
+
         clean_path = os.path.join(self.dataset_path, 'CL')
         if os.path.exists(clean_path):
-            for root, _, files in os.walk(clean_path):
-                self.img_paths = [f for f in files if f.lower().endswith('.png')]
+            # recursively gather all .png files under CL directory
+            self.img_paths = [f for f in os.listdir(clean_path) if f.lower().endswith('.png')]
+        else:
+            raise RuntimeError(f'Cannot find CL directory for split "{self.split}" at {clean_path}')
 
     def _load_data(self, data_idx):
         file_name = self.img_paths[data_idx]
-
-        clean_img = self._load_img(os.path.join(self.dataset_path, 'CL', file_name))
+        clean_img_path = os.path.join(self.dataset_path, 'CL', file_name)
+        clean_img = self._load_img(clean_img_path)
 
         return {'clean': clean_img}
